26c55b9475dbfa44d85b678e3cc4100b84d66070
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-14 15:47:45 +0200

    creation file + inserting variables

---
+++ main.js
@@ -0,0 +1,11 @@
+/* richiesta dati per calcolo biglietto */
+// variabile per chiedere i chilometri che vuole percorrere
+const userKm = (prompt ('How many kilometers do you want to go?'));
+console.log(userKm)
+
+// variabile per chiedere l'età dell'utente, così da capire se possibile applicare lo sconto
+const userAge = parseInt(prompt ('According to age there are special discounts, how old are you?'));
+console.log(userAge);
+
+/* eseguiamo i calcoli per il costo biglietto */
+// controllo che i dati inseriti siano corretti 
