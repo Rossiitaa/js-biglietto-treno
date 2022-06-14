e286bed28c849caad28a1feaf05e695632da04ef
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-14 17:37:02 +0200

    correction error

--- main.js
+++ main.js
@@ -10,19 +10,21 @@ console.log(userAge);
 /* eseguiamo i calcoli per il costo biglietto */
 const priceKm = 0.21;
 let NotDiscountedPrice = userKm * priceKm;
-console.log('The cost of your ticket is ' + NotDiscountedPrice.toFixed(2));
+
 // controllo che i dati inseriti siano corretti 
-if ( (isNaN(userKm, userAge) == true) || (userKm, userAge < 0) ) {
-    console.log('Error, number not accepted, reload the page')
+if ((isNaN(userKm) == true)  || (isNaN(userAge) == true) ||  (userKm < 0) || (userAge < 0)) {
+    console.log('Error, number not accepted, reload the page');
     // inserisco gli sconti in base all'età
     } else {
         if (userAge < 18) {
-        let DiscountedPrice = NotDiscountedPrice * 20 / 100 
+        let DiscountedPrice = NotDiscountedPrice - NotDiscountedPrice * 20 / 100;
         console.log('The price of your ticket is ' + NotDiscountedPrice.toFixed(2), 
         'but because you are a minor you received a discount, the price of the discounted ticket is ' + DiscountedPrice.toFixed(2));
     } else if (userAge > 65) {
-        let DiscountedPrice = NotDiscountedPrice * 40 / 100 
+        let DiscountedPrice = NotDiscountedPrice - NotDiscountedPrice * 40 / 100; 
         console.log('The price of your ticket is ' + NotDiscountedPrice.toFixed(2), 
         'but since you are over 65 years old you received a discount, the price of the discounted ticket is ' + DiscountedPrice.toFixed(2));
+    } else {
+        console.log('The price of your ticket is ' +NotDiscountedPrice.toFixed(2));
     }
     }
